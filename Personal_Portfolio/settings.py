32da600cf645b5c3dadfad7ce585949bd5165f37
Wafa P M <pmwafa@gmail.com>
2022-04-30 00:44:33 +0530

    changes made

--- Personal_Portfolio/settings.py
+++ Personal_Portfolio/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewarw',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Personal_Portfolio.urls'
